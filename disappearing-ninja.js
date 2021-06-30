ad936a69d00c5f347f548b8c7d895dbeb0c66c1a
Sevrinn <sevrinnsevrinn@gmail.com>
2021-06-30 06:35:01 -0700

    finished assignment

---
+++ disappearing-ninja.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.pika').click(function () {
+    $(this).fadeOut();
+  });
+});
+
+//1: makes sure document is ready before running code inside
+//2: targets elements with .pika class and puts click event listenter on them
+//3: targets the element that was clicked and fades it out
+//4: closes event listenter
+//5: closes ready event.
